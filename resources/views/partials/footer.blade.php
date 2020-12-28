2558bfb9bcbe1eda0b2320c69784a0fdc961eaf4
Massimiliano Ambrogio <maxi.ambrogio@gmail.com>
2020-12-28 18:34:38 +0100

    Project 46 parte 1.4 fix html laravel scss

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,26 +2,57 @@
     FOOTER
 --}}
 <footer>
-    <div class="cont-img">
+    <div class="cont-text2">
+        {{-- Text 2 --}}
+        <h2 class="title1">Pastificio</h2>
+        <p class="text-1">Il Pastificio</p>
+        <p class="text-2">Grano decorticato a pietra</p>
+        <p class="text-3">Il Molise c'è</p>
+        <p class="text-4">Filiera Integrata</p>
+        <p class="text-5">100 anni di pasta</p>
+        <p class="text-6">Sartoria della pasta</p>
+        <p class="text-7">Spaghetto Quadrato</p>
+        <p class="text-8">Le Persone</p>
+        {{-- Text 3  --}}
+        <h2 class="title2">Prodotti</h2>
+        <p class="text-9">Le classiche</p>
+        <p class="text-10">Le Integrali</p>
+        <p class="text-11">Le Speciali</p>
+        <p class="text-12">Le Biologiche</p>
+        <p class="text-13">Le Gluten-Free</p>
+        <p class="text-14">Le Semole</p>
+        <p class="text-15">Le Extra di Lusso</p>
+        {{-- Text 4  --}}
+        <h2 class="title3">Collezione da chef</h2>
+        <p class="text-16">Collezione da chef</p>
+        <p class="text-17">Grandi Cucine</p>
+        <p class="text-18">Biologiche</p>
+        <p class="text-19">Quadrate</p>
+    </div>
+
+    <div class="container"></div>
+    
+    <div class="cont-text1">
+        {{-- Logo Molisana --}}
         <a href="{{ route('home') }}" class="brand">
             <img src="{{ asset('img/logo.png') }}" alt="Logo Molisana">
         </a>
-    </div>
-    <div class="cont-text1">
+        {{-- Text 1 --}}
         <p class="text-1">Ragione sociale: La Molisana S.P.A.</p>
-         <p class="text-2">Sede legale: Contrada Colle delle Api, 100/A</p>
-         <p class="text-3">86100 - Campobasso(CB)</p>
-         <p class="text-4">Pec: lamolisana@pec.it</p>
-         <p class="text-5">Tel: +39 0874 4981</p>
-         <p class="text-6">Fax: +39 0874 629584</p>
-         <p class="text-7">info@lamolisana.it (per segnalazioni degli utenti)</p>
-         <p class="text-8">commerciale@lamolisana.it</p>
-         <p class="text-9">export@lamolisana.it</p>
-         <p class="text-10">numero verde 800818081 </p>
-         <p class="text-11">telefono 3801292455</p>
+        <p class="text-2">Sede legale: Contrada Colle delle Api, 100/A</p>
+        <p class="text-3">86100 - Campobasso(CB)</p>
+        <p class="text-4">Pec: lamolisana@pec.it</p>
+        <p class="text-5">Tel: +39 0874 4981</p>
+        <p class="text-6">Fax: +39 0874 629584</p>
+        <p class="text-7">info@lamolisana.it (per segnalazioni degli utenti)</p>
+        <p class="text-8">commerciale@lamolisana.it</p>
+        <p class="text-9">export@lamolisana.it</p>
+        <p class="text-10">numero verde 800818081 </p>
+        <p class="text-11">telefono 3801292455</p>
     </div>
 
-    {{-- <div class="cont-img">
-        <img src="./img/footer-montagne-bottom.jpg" alt="Footer Montagne">
-    </div> --}}
+    {{-- Footer Montagne --}}
+    <div class="cont-footer-img">
+        <img src="./img/footer-montagne-bottom.jpg" alt="Montagne">
+    </div>
 </footer>
